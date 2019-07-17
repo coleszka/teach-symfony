1985a71cdadc464e53d6a162dc6aec7bea695053
coleszka <symon@interia.eu>
2019-07-17 15:20:16 +0200

    add controller displaing notes and edit creater notes controller

---
+++ src/Controller/DisplayNotesController.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Entity\Notes;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class DisplayNotesController extends AbstractController
+{
+    /**
+     * @return Response
+     * @Route("/mynotes", name="my_notes")
+     *
+     */
+    public function displayNotes()
+    {
+        $notes = $this->getDoctrine()->getRepository(Notes::class)->findBy(
+            [
+                'user' => $this->getUser(),
+                ]
+        );
+        //dump($notes);
+        return $this->render('notes/display_notes.html.twig',
+            array('notes' => $notes));
+    }
+}
--- src/Controller/NotesController.php
+++ src/Controller/NotesController.php
@@ -4,12 +4,15 @@
 namespace App\Controller;
 
 use App\Entity\Notes;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Form\FormTypeInterface;
+use function Symfony\Component\Validator\Tests\Constraints\choice_callback;
 
 
 class NotesController extends AbstractController
@@ -28,11 +31,24 @@ class NotesController extends AbstractController
         $note = new Notes();
         $note->setUser($user);
         $note->setCreateDate(new \DateTime('now'));
-        $note->setCategory('kategoria');
+
 
         $form = $this->createFormBuilder($note)
-            ->add('name', TextType::class)
-            ->add('description', TextType::class)
+            ->add('name', TextType::class, ['required'   => true])
+            ->add('description', TextareaType::class, [
+                'required'   => true,
+                'attr' => [
+                    'rows' => 1,
+                    'cols' => 30
+                ]])
+
+            ->add('category', ChoiceType::class, [
+                'choices' => [
+                    'PHP' => 'php',
+                    'SQL' => 'sql',
+                    'Symfony' => 'symfony',
+                    'Ubuntu' => 'ubuntu',
+                    'Windows' => 'windows',]])
             ->add('save', SubmitType::class, ['label' => 'Create Note'])
             ->getForm();
 
@@ -42,6 +58,8 @@ class NotesController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($note);
             $entityManager->flush();
+
+            return $this->redirectToRoute('my_notes');
         }
 
         return $this->render('notes/new.html.twig', [
